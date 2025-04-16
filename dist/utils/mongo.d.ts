04749e758c7f29b8c2950568db2342ba81e7251d
sulabh shukla <sulabh9999@gmail.com>
2025-04-16 13:46:00 +0530

    mongo utils added 1

---
+++ dist/utils/mongo.d.ts
@@ -0,0 +1,22 @@
+export declare const MongoUtils: {
+    /**
+       get:
+  input: '314142'
+  output: '314142'
+  input: 'addr_314142'
+  output: '314142'
+  input: 'addr_pi_314142'
+  output: '314142'
+       */
+    get: (id: string) => string;
+    /**
+     set:
+  input: 'addr', '314142'
+  output: 'addr_314142'
+  input: 'addr', 'addr_314142'
+  output: 'addr_314142'
+  input: 'addr', 'pi_314142'
+  output: 'addr_314142'
+     */
+    set: (prefix: string, id: string) => string;
+};
---
+++ dist/utils/mongo.js
@@ -0,0 +1,31 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.MongoUtils = void 0;
+exports.MongoUtils = {
+    /**
+       get:
+  input: '314142'
+  output: '314142'
+  input: 'addr_314142'
+  output: '314142'
+  input: 'addr_pi_314142'
+  output: '314142'
+       */
+    get: (id) => {
+        const parts = id.split("_");
+        return parts[parts.length - 1];
+    },
+    /**
+     set:
+  input: 'addr', '314142'
+  output: 'addr_314142'
+  input: 'addr', 'addr_314142'
+  output: 'addr_314142'
+  input: 'addr', 'pi_314142'
+  output: 'addr_314142'
+     */
+    set: (prefix, id) => {
+        const cleanId = exports.MongoUtils.get(id); // remove any prefix
+        return `${prefix}_${cleanId}`;
+    },
+};
